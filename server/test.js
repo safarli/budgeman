19326eec211c77fd9491b23ab454b6e3fb22938d
Ofis PC <binnacles@gmail.com>
2021-03-17 18:44:08 +0400

    test code added to test.js

--- server/test.js
+++ server/test.js
@@ -1,21 +1,13 @@
-const bcryptjs = require('bcryptjs')
+const programStart = Date.now()
 
-function test_1() {
-    try {
-        setTimeout(() => {
-            throw new Error('JUST ERROR')
-        }, 4000)
-    } catch (e) {
-        console.log(`ERROR OCCURED ${e}`)
-    }
+while(Date.now() - programStart < 1500) {
+    // console.log(Date.now())
 }
 
-const mystr = "safarlee"
-
-const salt = bcryptjs.genSaltSync(10)
-const hash = bcryptjs.hashSync(mystr)
-
-const compareResult = bcryptjs.compareSync(mystr, hash)
-
-console.log(hash)
-console.log(compareResult)
+setTimeout(
+    () => {
+        const delay = Date.now() - programStart
+        console.log(delay)
+    },
+    300
+)
