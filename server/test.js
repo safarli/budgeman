dbcf93142fd309019a0009d01cc8521100c58f23
safarli <wentonic@gmail.com>
2021-03-19 00:48:08 +0400

    a commit

--- server/test.js
+++ server/test.js
@@ -1,5 +1,26 @@
-const {Worker} = require('worker_threads')
+// const { Worker } = require('worker_threads')
 
-const workerFile = './worker1.js'
-const worker1 = new Worker(workerFile)
-worker1.on('message', (msg) => console.log(msg))
+// const workerFile = './worker1.js'
+// const worker1 = new Worker(workerFile)
+// worker1.on('message', msg => console.log(msg))
+
+
+let blue = true
+
+
+const loopInterval = setInterval(
+    () => {
+        if (!blue) {
+            clearInterval(loopInterval)
+            return
+        }
+        console.log('Looping...')
+    }, 700
+)
+
+setTimeout(
+    () => {
+        blue = !blue
+    },
+    6000
+)
